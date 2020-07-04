8acc6e15821db86df997e17eb90c156e4f3a42f9
Meldiron <matejbacocom@gmail.com>
2020-07-04 16:34:47 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ɵCodegenComponentFactoryResolver,
+  OnInit,
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'kucharka';
+export class AppComponent implements OnInit {
+  data = null;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.httpClient.get('assets/config.json').subscribe((data) => {
+      this.data = data;
+    });
+  }
+
+  scrollTo(tableId: string) {
+    document.getElementById(tableId).scrollIntoView();
+  }
+
+  getStyle(table: any, recordI: number, rowI: number): any {
+    const obj = {
+      fontWeight: 'normal',
+      textTransform: 'none',
+    };
+
+    const lineConfig = table.headings[rowI];
+
+    if (!lineConfig) {
+      return obj;
+    }
+
+    if (lineConfig.isBold) {
+      obj.fontWeight = 'bold';
+    }
+
+    if (lineConfig.isUppercase) {
+      obj.textTransform = 'uppercase';
+    }
+
+    return obj;
+  }
 }
